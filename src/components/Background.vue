6dac622c69a381307d9f3d692e06d4dc8b7f7d76
Derik Linch <dlinch33@gmail.com>
2019-02-06 22:24:39 -0700

    updated eslint, built background vue component

---
+++ src/components/Background.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="background" :style="{'backgroundImage': `url(${require('@/assets/black_beach.jpg')})`}">
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+.background {
+  background-image: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  left: 0;
+  min-height: 50rem;
+  position: relative;
+  top: 0;
+  width: 100%;
+  z-index:  -1;
+}
+</style>
+
+<script>
+export default {
+  name: 'Background',
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
+    <Background />
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import Background from '@/components/Background.vue';
+
+export default {
+  components: {
+    Background,
+  },
+};
+</script>
