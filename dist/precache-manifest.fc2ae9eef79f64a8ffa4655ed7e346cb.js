5deef8214e566fd52b0e817f66f71451e829b243
Derik Linch <dlinch33@gmail.com>
2019-03-14 18:50:22 -0600

    commit /dist folder with node server

---
+++ dist/precache-manifest.fc2ae9eef79f64a8ffa4655ed7e346cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "37e125d3fd40981ba5d5",
+    "url": "/css/about.bce48375.css"
+  },
+  {
+    "revision": "37e125d3fd40981ba5d5",
+    "url": "/js/about.e0b1247c.js"
+  },
+  {
+    "revision": "62089c4d980aeec7dd9d",
+    "url": "/css/app.c2ba204a.css"
+  },
+  {
+    "revision": "62089c4d980aeec7dd9d",
+    "url": "/js/app.5a733abb.js"
+  },
+  {
+    "revision": "edc7703fd974df383c38",
+    "url": "/js/chunk-vendors.ff1c86d6.js"
+  },
+  {
+    "revision": "d62fa7dc3fecaa169c1beee8c5b26110",
+    "url": "/img/black_beach.d62fa7dc.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "95ff5278bc1275a1b91ce110eba1b592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
