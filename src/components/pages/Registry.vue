51a91a567e1519f8803afdf88b66394bf8f20f56
Derik Linch <dlinch33@gmail.com>
2019-04-22 22:23:13 -0600

    added zola embed to site

--- src/components/pages/Registry.vue
+++ src/components/pages/Registry.vue
@@ -2,6 +2,12 @@
   <div class="registry">
     <h2>REGISTRY</h2>
     <h4>We are registered at the following stores:</h4>
+    <a
+      class="zola-registry-embed"
+      href="https://www.zola.com/registry/derikandashley"
+      data-registry-key="derikandashley">
+      Our Zola Wedding Registry
+    </a>
     <div class="footer">Ashley & Derik | 9.13.2019</div>
   </div>
 </template>
